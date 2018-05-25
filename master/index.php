6e96d0b01ccdff5c17977ca5795594db70a5edf7
Marjorie Celestino <marjorie_celestino@hotmail.com>
2018-05-24 23:33:05 -0300

    corrections

---
+++ master/index.php
@@ -0,0 +1,86 @@
+<?php include 'header.php'; ?>
+
+	<div class="main">
+		<div class="time">
+			<h1 id="clock"><time>00:00:00</time></h1>
+		</div>
+		<div class="time-actions">
+			<a id="start" href="#" onclick="startClock(); hideFeedback();">
+				<img class="icons" src="images/play.png" alt="start">
+			</a>
+			<a id="pause" href="#" onclick="pauseClock(); hideFeedback();">
+				<img class="icons" src="images/pause.png" alt="pause">
+			</a>
+			<a id="stop" href="#" onclick="clearClock(); hideFeedback();">
+				<img class="icons" src="images/resume.png" alt="resume">
+			</a>
+			<a href="#" onclick="displayForm(); hideFeedback();">
+				<img class="icons" src="images/add.png" alt="new-task" title="Book time">
+			</a>	
+		</div>
+
+		<div id="add-task-form" class="form-new-task" style="display:none;">
+			<?php 
+
+				$today = date("Y-m-d");
+
+			?>
+			<form action="#" method="post">
+				<input type="text" name="name" placeholder="Name" maxlength="100" required>
+				<input type="date" name="date" placeholder="mm/dd/aaaa" value="<?php echo $today; ?>" required>
+				<input id="time" type="text" name="time" placeholder="hh:mm:ss" value="" maxlength="8" required>
+				<textarea type="text" name="description"  placeholder="..." maxlength="500" required></textarea>
+				<input type="submit" name="submit" value="book" onclick="resetTime(); displayFeedback();" />
+			</form>
+		</div>
+		<div id="feedback" class="feedback" style="display:block;">
+
+			<?php  
+
+				function clean_input($input){
+	                $input = trim($input);
+	                $input = stripslashes($input);
+	                $input = htmlspecialchars($input);
+
+	                return $input;
+	            }
+
+				if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+					$name = $_POST['name'];
+					$date = $_POST['date'];
+					$time = $_POST['time'];
+					$description = $_POST['description'];
+
+					$name = clean_input($name);
+					$date = clean_input($date);
+					$time = clean_input($time);
+					$description = clean_input($description);
+
+					echo $date;
+
+					//Insert data to DB
+				    if ($db_connect->connect_error) {
+				        echo 'Error: ' . $db_connect->connect_error;
+				    } else {
+
+				    	$sql = "INSERT INTO tasks (name, end_date, end_time, description) VALUES ('$name', '$date', '$time', '$description')";
+
+				    	if ($db_connect->query($sql)) {
+	                    
+		                    echo "<p>Task <span>" . $name . "</span> booked successfully </p>";
+
+		                }else{
+
+		                    echo "<p>Unable to book the task <span>" . $name . "</span></p>";
+		                    //echo mysqli_error($db_connect) . "<br><br>";
+		                }
+				    }
+
+				}
+			?>
+			
+		</div>
+	</div>
+
+<?php include 'footer.php'; ?>
---
+++ master/tasks.php
@@ -0,0 +1,114 @@
+<?php include 'header.php'; ?>
+
+	<div class="main">
+
+		<div id="search" class="search">
+			<form action="">
+				<input type="search" placeholder="Search.." name="search">
+				<button type="submit">Go</button>
+			</form>
+		</div>
+
+		<?php  
+
+			//get search value and clean input
+			$search = $_GET['search'];
+			$search = strtolower($search);
+			$search = trim($search);
+	        $search = stripslashes($search);
+	        $search = htmlspecialchars($search);
+
+	        //pagination variables
+	      	$total_itens = 1;
+	      	$limit = 5;
+
+	      	if (isset($_GET["page"])) { 
+	      		$page  = $_GET["page"]; 
+	      	} else { 
+	      		$page=1; 
+	      	} 
+
+			$start_from = ($page-1) * $limit;
+
+			//fetches the data and displays it on the page
+			if ($db_connect->connect_error) {
+                echo 'Error: ' . $db_connect->connect_error;
+            } else {
+
+            	if($search !== null && $search !== ""){
+            		$sql = "SELECT * FROM tasks ORDER BY `id` DESC";
+
+            	}else{
+            		$sql = "SELECT * FROM tasks ORDER BY `id` DESC LIMIT $start_from, $limit";
+
+            		//finds out total number of itens
+					$result = $db_connect->query("SELECT * FROM tasks");
+					$total_itens = $result->num_rows;
+            	}
+
+            	$result = $db_connect->query($sql);
+
+            	$total_rows = $result->num_rows;
+
+            	if ($total_rows > 0) {
+
+            		while ($row = $result->fetch_assoc()) { 
+
+            			//searches description
+            			if($search !== null && $search !== ""){
+							if(strpos($row['description'], $search) === false){
+								continue;
+							}
+						}
+        ?>
+
+            			<div class="row">
+						  <div class="column left">
+						    <img src="images/task.png">
+						  </div>
+						  <div class="column middle">
+						    <h2><?php echo $row['name'] ?></h2>
+						    <p><?php echo $row['description'] ?></p>
+						  </div>
+						  <div class="column right">
+						    <h3>Finished at</h3>
+						    <p><?php echo $row['end_date'] ?></p>
+						    <p><?php echo $row['end_time'] ?></p>
+						  </div>
+						</div>
+
+
+        <?php	
+            		}
+
+            	}else{ 
+        ?>
+            		<div class="feedback">
+            			<p><?php echo "There are no tasks booked" ?></p>
+            		</div>
+        <?php	
+        		}
+        ?>
+        			<div id="pages" class="pagination">
+		  				<a class="page" href="tasks.php">&laquo;</a>
+
+        <?php
+        		//defines number of pages
+        		$total_pages = ceil($total_itens / $limit);
+
+        		for ($i=1; $i<=$total_pages; $i++) {
+        ?>
+
+        				<a class="page" id="<?php echo $i; ?>" href="tasks.php?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+
+        <?php
+        		}
+
+            }
+		?>
+					  	<a class="page" href="tasks.php?page=<?php echo $total_pages; ?>">&raquo;</a>
+					</div>
+	
+	</div>
+	
+<?php include 'footer.php'; ?>
